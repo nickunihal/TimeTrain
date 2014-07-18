037947db21c27f7ec1cad95730614fabb6c064a0
Nihal Nizar <nihal@qburst.com>
2014-07-18 16:46:32 +0530

    gplud update again n again

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,29 +10,28 @@ class Member < ActiveRecord::Base
   def self.update_records (user)
   	TwitterModel.store_urls(user) if user.twitter_id.present? 
     if user.gplus_id.present?
-      if user.token_expired?
-        data = {
-                 client_id: '476602585408-3fiklaclekbinfmbd2lsdjcur1u21ril.apps.googleusercontent.com',
-                 client_secret: 'RX-7Mq_SXT1DJSJsYJtLj6a4',
-                 refresh_token: user.gplus_refresh_token,
-                 grant_type: "refresh_token"
-               }
-        @response = ActiveSupport::JSON.decode(RestClient.post "https://accounts.google.com/o/oauth2/token", data)
-        if @response["access_token"].present?
-          user.gplus_token = @response["access_token"]
-          user.gplus_token_expires_at = @response["expires_in"]
-          user.save
-        end
-
-      end
-  	  GoogleModel.store_urls(user) 
+      user.token_expired?
+      GoogleModel.store_urls(user) 
     end
   end
 
   def token_expired?
     expiry = Time.at(self.gplus_token_expires_at.to_i) 
-    return true if expiry < Time.now 
-    false
+    if expiry < Time.now 
+      data = {
+              client_id: '476602585408-3fiklaclekbinfmbd2lsdjcur1u21ril.apps.googleusercontent.com',
+              client_secret: 'RX-7Mq_SXT1DJSJsYJtLj6a4',
+              refresh_token: self.gplus_refresh_token,
+              grant_type: "refresh_token"
+             }
+      @response = ActiveSupport::JSON.decode(RestClient.post "https://accounts.google.com/o/oauth2/token", data)
+      if @response["access_token"].present?
+        self.gplus_token = @response["access_token"]
+        self.gplus_token_expires_at = @response["expires_in"]
+        self.username = "updated"
+        self.save
+      end
+    end
   end
   
 end
