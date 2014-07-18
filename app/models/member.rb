2a3d138d4c801b50bb09c6682146e1258a6570e1
Nihal Nizar <nihal@qburst.com>
2014-07-18 18:09:26 +0530

    ooh

--- app/models/member.rb
+++ app/models/member.rb
@@ -16,25 +16,18 @@ class Member < ActiveRecord::Base
   end
 
   def token_expired?
-  #  expiry = Time.at(self.gplus_token_expires_at.to_i) 
-    #if expiry < Time.now 
-      data = {
+    data = {
               client_id: '476602585408-3fiklaclekbinfmbd2lsdjcur1u21ril.apps.googleusercontent.com',
               client_secret: 'RX-7Mq_SXT1DJSJsYJtLj6a4',
               refresh_token: self.gplus_refresh_token,
               grant_type: "refresh_token"
              }
-      @response = ActiveSupport::JSON.decode(RestClient.post "https://accounts.google.com/o/oauth2/token", data)
-      if @response["access_token"].present?
-        self.gplus_token = @response["access_token"]
-        self.gplus_token_expires_at = @response["expires_in"]
-        self.save
-      end
-    rescue
-      #rescue from bad connection
-      #google refresh token 
-   # end
-   # end
+    @response = ActiveSupport::JSON.decode(RestClient.post "https://accounts.google.com/o/oauth2/token", data)
+    if @response["access_token"].present?
+      self.gplus_token = @response["access_token"]
+      self.gplus_token_expires_at = @response["expires_in"]
+      self.save
+    end
   end
   
 end
